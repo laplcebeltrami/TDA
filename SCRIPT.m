888be2be4cf5ba31f44cf9a8fb4db852de4ff656
laplcebeltrami <68470151+laplcebeltrami@users.noreply.github.com>
2022-08-15 23:32:05 -0500

    Add files via upload

--- SCRIPT.m
+++ SCRIPT.m
@@ -1,27 +1,29 @@
-%Downloaded from https://osf.io/hnj7s/
+% The script explains how to compute and display monotone Betti-0 
+% and Betti-1 curves over graph filtations introduced in
+%
+% Chung, M.K., Lee, H. Ombao. H., Solo, V. 2019 Exact topological inference 
+% of the resting-state brain networks in twins, Network Neuroscience 3:674-694
+% https://pages.stat.wisc.edu/~mchung/papers/chung.2019.NN
+%
+% Chung, M.K., Huang, S.-G., Gritsenko, A., Shen, L., Lee, H. 2019 Statistical 
+% inference on the number of cycles in brain networks. IEEE International 
+% Symposium on Biomedical Imaging (ISBI) 113-116 
+% https://pages.stat.wisc.edu/~mchung/papers/chung.2019.ISBI.pdf
+%
+%
+% (C) 2022 Moo K. Chung
+%  University of Wisconsin-Madison
+%
+% The codes are downloaded from https://github.com/laplcebeltrami/TDA
 
-gng_inh=zeros(100,100,144);
-for i=1:144
-    subid = sprintf('%03d',i);
-    filename = strcat('Schaefer100_17_sub-',subid, '_gng_inhibition.txt');
-    a= load(filename);
-    gng_inh(:,:,i)=a;
-end
 
-
-gng_ini=zeros(100,100,144);
-for i=1:144
-    subid = sprintf('%03d',i);
-    filename = strcat('Schaefer100_17_sub-',subid, '_gng_initiation.txt');
-    a= load(filename);
-    gng_ini(:,:,i)=a;
-end
-
-%save gng.mat gng_inh gng_ini
-
+load gng.mat %gng.mat contains variables gng_inh gng_ini
 %gng_inh is 100 by 100 connectivity matrix of inhibition task for 144
 %subjects. gng_ini is 100 by 100 connectivity matrix of initiation task for 144
 %subjects. The same subject are used for two different tasks. 
+
+%-----------
+%Computing Betti-0 curves
 
 thresholds=[0:0.01:1];
 betti0=[];
@@ -29,14 +31,17 @@ for i=1:114
     [beta0, beta1, biggest0, biggest1] = PH_betti3(gng_ini(:,:,i), thresholds);
     betti0=[betti0; beta0];
 end
-mean_ini = mean(betti0,1);
+mean_ini = mean(betti0,1); %average Betti-0 curve
+
 betti0=[];
 for i=1:114
     [beta0, beta1, biggest0, biggest1] = PH_betti3(gng_inh(:,:,i), thresholds);
     betti0=[betti0; beta0];
 end
-mean_inh = mean(betti0,1);
+mean_inh = mean(betti0,1); %average Betti-0 curve  
 
+
+%Visualizing Betti-0 curves
 figure; 
 subplot(1,2,1);
 plot(thresholds,mean_ini, 'k', 'LineWidth',2,'LineStyle','-')
@@ -45,11 +50,11 @@ legend('Initiation', 'inhibition')
 set(gca,'FontSize',25);
 legend boxoff 
 xlabel('Correlations');
-ylabel('\beta_0');
+ylabel('\beta_0'); box on;
 
 
-%----------
-%Betti-1
+%-----------
+%Computing Betti-1 curves
 
 thresholds=[0:0.01:1];
 betti1=[];
@@ -57,16 +62,16 @@ for i=1:114
     [beta0, beta1, biggest0, biggest1] = PH_betti3(gng_ini(:,:,i), thresholds);
     betti1=[betti1; beta1];
 end
-mean_ini = mean(betti1,1);
+mean_ini = mean(betti1,1); %average Betti-1 curve
 
 betti1=[];
 for i=1:114
     [beta0, beta1, biggest0, biggest1] = PH_betti3(gng_inh(:,:,i), thresholds);
-    betti1=[betti1; beta1];
+    betti1=[betti1; beta1]; 
 end
-mean_inh = mean(betti1,1);
+mean_inh = mean(betti1,1); %average Betti-1 curve
 
-
+%Visualizing Betti-1 curves
 subplot(1,2,2);
 hold on; plot(thresholds,mean_ini, 'k', 'LineWidth',2,'LineStyle','-')
 hold on; plot(thresholds,mean_inh, 'r', 'LineWidth',2,'LineStyle','--')
@@ -74,6 +79,5 @@ legend('Initiation', 'inhibition')
 set(gca,'FontSize',25);
 legend boxoff 
 xlabel('Correlations');
-ylabel('\beta_1');
-
+ylabel('\beta_1'); box on;
 figure_bg('w')
